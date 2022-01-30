4506b737eff161589142ec013a96f92ac2638936
arstanbektemirov <abektemirov82@gmail.com>
2022-01-30 14:45:12 -0500

    commit from test1 branch

--- src/test/java/com/cydeo/tests/Test1.java
+++ src/test/java/com/cydeo/tests/Test1.java
@@ -4,6 +4,7 @@ public class Test1 {
     // first commit
     public static void main(String[] args) {
         System.out.println("How its work?");
+        System.out.println("start understanding a little bit");
 
     }
 }
